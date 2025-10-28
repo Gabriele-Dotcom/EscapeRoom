db26b6e067a98b59ed1f4570d58ca31b902f2c88
Gabriele-Dotcom <108992308+Gabriele-Dotcom@users.noreply.github.com>
2025-10-28 18:50:35 +0100

    Partially fixed credits
    Credits is now visible but the background is transparent

--- Assets/scripts/about_panel.cs
+++ Assets/scripts/about_panel.cs
@@ -3,11 +3,11 @@ using UnityEngine;
 public class about_panel : MonoBehaviour
 {
     public GameObject aboutPanel;
-
     // Mostra il pannello
     public void ShowAbout()
     {
         aboutPanel.SetActive(true);
+        
     }
 
     // Nasconde il pannello
