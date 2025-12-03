72f9c3b737eaf9d19cf7225f0cfd78e458b78636
Gabriele-Dotcom <108992308+Gabriele-Dotcom@users.noreply.github.com>
2025-12-03 10:13:41 +0100

    added touch screen compatibility

--- Assets/scripts/InputHandler.cs
+++ Assets/scripts/InputHandler.cs
@@ -14,7 +14,7 @@ public class InputHandler : MonoBehaviour
     {
         if(!context.started) return;
 
-        var rayHit = Physics2D.GetRayIntersection(_mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue()));
+        RaycastHit2D rayHit = Physics2D.GetRayIntersection(_mainCamera.ScreenPointToRay(Pointer.current.position.ReadValue()));
         if(!rayHit.collider) return;
 
         rayHit.collider.GetComponent<ShowHint>()?.ShowHintPanel();
